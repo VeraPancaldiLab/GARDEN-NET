ee4872df4cb4f1c7d1410c730b6f6d6695da4f75
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2018-12-17 16:33:10 +0100

    Submit on Enter key pressed event

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -18,13 +18,20 @@ export class SearchPanel extends React.Component<any, any> {
     this.props.onRangeChange('Choose')
   }
 
+  handleEnterKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      this.onSubmit()
+    }
+  }
+
   render() {
 
     return (
       <Form className='text-center'>
         <FormGroup>
           <Label for="searcher">Search</Label>
-          <Input className='text-center' type="text" name="Search" placeholder="Hoxa1 6:52155590 6:52155590-52158317" onChange={this.onInputChange} />
+          <Input className='text-center' type="text" name="Search" placeholder="Hoxa1 6:52155590 6:52155590-52158317" onChange={this.onInputChange} onKeyPress={this.handleEnterKey} />
         </FormGroup>
         <Button disabled={this.props.text == ''} style={{marginBottom: '15px'}} onClick={this.onSubmit}>Submit</Button>
       </Form>
