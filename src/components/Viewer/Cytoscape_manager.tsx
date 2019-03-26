f81816809be7b3ae8a3a86613ee1bdfbc14bfd48
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-03-26 18:03:37 +0100

    Show neighbourhood of ranges in left view using 'searched' node variable

--- src/components/Viewer/Cytoscape_manager.tsx
+++ src/components/Viewer/Cytoscape_manager.tsx
@@ -194,7 +194,8 @@ export class Cytoscape_manager extends React.Component<any, any> {
       };
       const updateNeighbourhood = (cy: any) => {
         if (this.state.neighbourhood_node_id !== undefined) {
-          const left_node = cy.nodes().filter((node: any) => this.checkNode(node, this.state.neighbourhood_node_id))[0];
+          const left_node = this.left_cy_network.nodes().filter((node: any) => { const node_id = node.data("chr") + "_" + node.data("start"); return node_id.toLowerCase() == this.state.neighbourhood_node_id; })[0];
+
           if (left_node) {
             const neighbourhood = left_node.closedNeighbourhood();
             cy.fit(neighbourhood);
@@ -284,7 +285,7 @@ export class Cytoscape_manager extends React.Component<any, any> {
           .selector("node")
           .style(dict_style).update();
 
-        const right_node = this.right_cy_network.nodes().filter((node: any) => this.checkNode(node, search))[0];
+        const right_node = this.right_cy_network.nodes().filter((node: any) => node.data("searched"))[0];
 
         if (!right_node) {
           // This node searched is not found so exit inmmediately without crash
@@ -307,7 +308,7 @@ export class Cytoscape_manager extends React.Component<any, any> {
         const searched_chromosome = right_node.data("chr");
         // Force color the neighbourhood when the chromosome is the same
         if (this.props.chromosome === searched_chromosome || this.props.chromosome === "PP") {
-          const left_node = this.left_cy_network.nodes().filter((node: any) => this.checkNode(node, this.state.neighbourhood_node_id))[0];
+          const left_node = this.left_cy_network.nodes().filter((node: any) => node.data("searched"))[0];
           if (left_node) {
             const neighbourhood = left_node.closedNeighbourhood();
             this.left_cy_network.fit(neighbourhood);
@@ -406,14 +407,5 @@ export class Cytoscape_manager extends React.Component<any, any> {
         </div>
       </div>
     );
-  }
-
-  private checkNode = (node: any, search: string) => {
-    const node_id = node.data("chr") + "_" + node.data("start");
-    const search_id = search.split("-")[0].replace(":", "_");
-    if (node.data("curated_gene_name") === search || node_id.toLowerCase().startsWith(search_id.toLowerCase())) {
-      return node;
-    }
-    return null;
   }
 }
