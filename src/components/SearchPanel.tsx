2b44105f35629f9353e656b5680f7608da5f612f
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-06-04 20:17:33 +0200

    Add organism and cell type to the searcher

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Form, FormGroup, Input, Label, ListGroup, ListGroupItem } from "reactstrap";
+import { Alert, Button, Form, FormGroup, Input, ListGroup, ListGroupItem } from "reactstrap";
 
 export class SearchPanel extends React.Component<any, any> {
 
@@ -66,11 +66,18 @@ export class SearchPanel extends React.Component<any, any> {
   }
 
   public render() {
+    const organism: any = {Homo_sapiens: "Homo sapiens", Mus_musculus:"Mus musculus"};
 
+    const cell_type: any = {
+      Homo_sapiens: { aCD4: "Activated total CD4+T cells", EP: "Endothelial precursors", Ery: "Erythroblasts", FoeT: "Fetal thymus", Mac0: "Macrophages M0", Mac1: "Macrophages M1", Mac2: "Macrophages M2", MK: "Megakaryocytes", Mon: "Monocytes", naCD4: "Non-activated total CD4+T cells", nCD4: "Naive CD4+ T cells", nCD8: "Naive CD8+ T cells", Neu: "Neutrophils", tCD4: "Total CD4+ T cells", tCD8: "Total CD8+ T cells" },
+      Mus_musculus: { Embryonic_stem_cells: "Embryonic stem cells" },
+    };
     return (
       <Form className="text-center">
         <FormGroup>
-          <Label for="searcher">Search</Label>
+        <Alert color="info">
+          {this.props.organism ? organism[this.props.organism] : ""} : {this.props.organism ? cell_type[this.props.organism][this.props.cell_type] : ""}
+      </Alert>
           <Input className="text-center" value={this.props.text} type="text" name="Search" placeholder="Hoxa1 6:52155590 6:52155590-52158317" onChange={this.onInputChange} onKeyPress={this.handleEnterKey} />
           <ListGroup className="text-center container-fluid" style={{ height: "auto", maxHeight: "110px", overflowX: "hidden", padding: "0px", display: ((this.props.text.length !== 0 && this.state.filtered_suggestions.length !== 0) ? "block" : "none") }}>
             {this.state.filtered_suggestions.map((suggestion: string) => <ListGroupItem key={suggestion} value={suggestion} onClick={this.onSuggestChange} style={{ padding: "0px", cursor: "pointer" }}>{this.boldText(suggestion, this.props.text.length)}</ListGroupItem>)}
