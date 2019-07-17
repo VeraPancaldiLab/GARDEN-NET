77e6ad528495791bc653f9b481faa5586cf25dc9
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-07-17 17:37:12 +0200

    Format code (there are not changes)

--- src/components/Header/MetadataPanel.tsx
+++ src/components/Header/MetadataPanel.tsx
@@ -1,25 +1,40 @@
 import * as React from "react";
-import { Button, Col, Label, Modal, ModalBody, ModalFooter, ModalHeader, Row, Table } from "reactstrap";
+import {
+  Button,
+  Col,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row,
+  Table
+} from "reactstrap";
 
 export class MetadataPanel extends React.Component<any, any> {
-
   private BASE_URL = "http://CRCT2107:8080/data/";
 
   constructor(props: any) {
     super(props);
-    this.state = { network_properties: {}, network_statistics: {}, metadata_error: false };
+    this.state = {
+      network_properties: {},
+      network_statistics: {},
+      metadata_error: false
+    };
   }
 
   public async fetchAsyncJson(url: string) {
     // Warning: The network file has to be serve before by a http server
     // http-server is provided to help to the development thanks to `yarn serve` command
     // In this case, the port used to serve is the 8080
-    return fetch(url).then((response) => {
-      const json = response.json();
-      return json;
-    }).catch((_err) => {
-        this.setState({metadata_error: true});
-    });
+    return fetch(url)
+      .then(response => {
+        const json = response.json();
+        return json;
+      })
+      .catch(_err => {
+        this.setState({ metadata_error: true });
+      });
   }
 
   // react-redux v7
@@ -28,31 +43,63 @@ export class MetadataPanel extends React.Component<any, any> {
   }
 
   public componentDidUpdate = (prevProps: any) => {
-    if (Object.keys(this.state.network_properties).length === 0 || this.props.chromosome !== prevProps.chromosome) {
-      this.fetchAsyncJson(this.BASE_URL + this.props.organism + "/" + this.props.cell_type + "/" + "metadata" + "/" + "chr" + this.props.chromosome + ".json").then((json) => {
-        this.setState({ network_properties: json.network_properties, network_statistics: json.network_statistics });
+    if (
+      Object.keys(this.state.network_properties).length === 0 ||
+      this.props.chromosome !== prevProps.chromosome
+    ) {
+      this.fetchAsyncJson(
+        this.BASE_URL +
+          this.props.organism +
+          "/" +
+          this.props.cell_type +
+          "/" +
+          "metadata" +
+          "/" +
+          "chr" +
+          this.props.chromosome +
+          ".json"
+      ).then(json => {
+        this.setState({
+          network_properties: json.network_properties,
+          network_statistics: json.network_statistics
+        });
       });
     }
-  }
+  };
 
   public render() {
     return (
       <div className="text-center" style={{ paddingTop: "15px" }}>
-        <Modal isOpen={this.state.metadata_error} centered={true} className="text-center">
+        <Modal
+          isOpen={this.state.metadata_error}
+          centered={true}
+          className="text-center"
+        >
           <ModalHeader>
-            <b className="text-danger" style={{marginLeft: "210px"}}>Error</b>
+            <b className="text-danger" style={{ marginLeft: "210px" }}>
+              Error
+            </b>
           </ModalHeader>
-          <ModalBody>
-            There is not metadata to be downloaded
-          </ModalBody>
+          <ModalBody>There is not metadata to be downloaded</ModalBody>
           <ModalFooter>
-            <Button color="danger" style={{marginRight: "200px"}} onClick={() => this.setState({metadata_error: false})}>Close</Button>
+            <Button
+              color="danger"
+              style={{ marginRight: "200px" }}
+              onClick={() => this.setState({ metadata_error: false })}
+            >
+              Close
+            </Button>
           </ModalFooter>
         </Modal>
-        <Label className="text-center">{this.props.chromosome === "PP" ? "Promoter-Promoter only" : "Chromosome " + this.props.chromosome} network data</Label>
+        <Label className="text-center">
+          {this.props.chromosome === "PP"
+            ? "Promoter-Promoter only"
+            : "Chromosome " + this.props.chromosome}{" "}
+          network data
+        </Label>
         <Row style={{ fontSize: "85%" }}>
           <Col xs={6}>
-            <Table bordered={true} size="sm" style={{fontSize: "x-small"}}>
+            <Table bordered={true} size="sm" style={{ fontSize: "x-small" }}>
               <thead>
                 <tr>
                   <th>Network properties</th>
@@ -60,12 +107,17 @@ export class MetadataPanel extends React.Component<any, any> {
                 </tr>
               </thead>
               <tbody>
-                {Object.keys(this.state.network_properties).map((key) => (<tr key={key}><td>{key}</td><td>{this.state.network_properties[key]}</td></tr>))}
+                {Object.keys(this.state.network_properties).map(key => (
+                  <tr key={key}>
+                    <td>{key}</td>
+                    <td>{this.state.network_properties[key]}</td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </Col>
           <Col xs={6}>
-            <Table bordered={true} size="sm" style={{fontSize: "x-small"}}>
+            <Table bordered={true} size="sm" style={{ fontSize: "x-small" }}>
               <thead>
                 <tr>
                   <th>Network statistics</th>
@@ -73,11 +125,21 @@ export class MetadataPanel extends React.Component<any, any> {
                 </tr>
               </thead>
               <tbody>
-                {Object.keys(this.state.network_statistics).map((key) => (<tr key={key}><td>{key}</td><td>{this.state.network_statistics[key]}</td></tr>))}
+                {Object.keys(this.state.network_statistics).map(key => (
+                  <tr key={key}>
+                    <td>{key}</td>
+                    <td>{this.state.network_statistics[key]}</td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </Col>
         </Row>
+        <div className="text-left">
+          Left click on nodes to navigate to WashU browser.
+          <br />
+          Right click on nodes to zoom into their neighborhood.
+        </div>
       </div>
     );
   }
--- src/components/Viewer/ControlPanel/SubPanels/FeaturesPanel.tsx
+++ src/components/Viewer/ControlPanel/SubPanels/FeaturesPanel.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
-import { ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle, Form, FormGroup, Label } from "reactstrap";
+import {
+  ButtonDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Form,
+  FormGroup,
+  Label
+} from "reactstrap";
 
 export class FeaturesPanel extends React.Component<any, any> {
-
   private BASE_URL = "http://CRCT2107:8080/data/";
 
   constructor(props: any) {
@@ -17,34 +24,43 @@ export class FeaturesPanel extends React.Component<any, any> {
 
   public componentDidUpdate = () => {
     if (this.props.features_list[0] == "") {
-      this.fetchAsyncJson(this.BASE_URL + this.props.organism + "/" + this.props.cell_type + "/" + "features.json").then((json) => {
+      this.fetchAsyncJson(
+        this.BASE_URL +
+          this.props.organism +
+          "/" +
+          this.props.cell_type +
+          "/" +
+          "features.json"
+      ).then(json => {
         this.props.onFeatureChange(json[0]);
         this.props.onFeaturesListChange(json);
       });
     }
-  }
+  };
 
   public async fetchAsyncJson(url: string) {
     // Warning: The network file has to be serve before by a http server
     // http-server is provided to help to the development thanks to `yarn serve` command
     // In this case, the port used to serve is the 8080
-    return fetch(url).then((response) => {
-      const json = response.json();
-      return json;
-    }).catch((_err) => {
-       this.props.onFeaturesListChange(null);
-    });
+    return fetch(url)
+      .then(response => {
+        const json = response.json();
+        return json;
+      })
+      .catch(_err => {
+        this.props.onFeaturesListChange(null);
+      });
   }
 
   public onFeatureChange = (event: any) => {
     this.props.onFeatureChange(event.target.value);
-  }
+  };
 
   public toggle = () => {
     this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
+      dropdownOpen: !this.state.dropdownOpen
     });
-  }
+  };
 
   public render() {
     const margin_style = {
@@ -54,22 +70,67 @@ export class FeaturesPanel extends React.Component<any, any> {
       borderWidth: "2px",
       paddingLeft: "5px",
       paddingRight: "5px",
-      fontSize: "small",
+      fontSize: "small"
     };
 
     return (
-      <div style={{ display: (this.props.features_list.length == 0 ? "none" : "block") }}>
+      <div
+        style={{
+          display: this.props.features_list.length == 0 ? "none" : "block"
+        }}
+      >
         <Form className="text-center" style={margin_style}>
           <FormGroup className="text-center">
-            <Label for="Select"><b>Features</b></Label>
+            <Label for="Select">
+              <b>Features</b>
+            </Label>
             <br />
-            <ButtonDropdown style={{ display: "grid" }} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-              <DropdownToggle style={{ color: "black", backgroundColor: "white", fontSize: "small" }} caret={true}>
+            <ButtonDropdown
+              style={{ display: "grid" }}
+              isOpen={this.state.dropdownOpen}
+              toggle={this.toggle}
+            >
+              <DropdownToggle
+                style={{
+                  color: "black",
+                  backgroundColor: "white",
+                  fontSize: "small"
+                }}
+                caret={true}
+              >
                 {this.props.feature}
               </DropdownToggle>
-              <DropdownMenu className="text-center container-fluid" style={{ height: "auto", maxHeight: "200px", overflowX: "hidden" }}>
-                {this.props.features_list.slice(0, -1).map((feature: string) => <div key={feature}><DropdownItem value={feature} onClick={this.onFeatureChange}>{feature}</DropdownItem><DropdownItem style={{ margin: 0 }} divider={true} /></div>)}
-                {this.props.features_list.slice(-1).map((feature: string) => <div key={feature}><DropdownItem value={feature} onClick={this.onFeatureChange}>{feature}</DropdownItem></div>)}
+              <DropdownMenu
+                className="text-center container-fluid"
+                style={{
+                  height: "auto",
+                  maxHeight: "200px",
+                  overflowX: "hidden"
+                }}
+              >
+                {this.props.features_list
+                  .slice(0, -1)
+                  .map((feature: string) => (
+                    <div key={feature}>
+                      <DropdownItem
+                        value={feature}
+                        onClick={this.onFeatureChange}
+                      >
+                        {feature}
+                      </DropdownItem>
+                      <DropdownItem style={{ margin: 0 }} divider={true} />
+                    </div>
+                  ))}
+                {this.props.features_list.slice(-1).map((feature: string) => (
+                  <div key={feature}>
+                    <DropdownItem
+                      value={feature}
+                      onClick={this.onFeatureChange}
+                    >
+                      {feature}
+                    </DropdownItem>
+                  </div>
+                ))}
               </DropdownMenu>
             </ButtonDropdown>
           </FormGroup>
