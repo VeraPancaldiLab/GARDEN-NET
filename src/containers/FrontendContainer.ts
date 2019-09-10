5bb733903a4452fbd0434e06ecea3cdb1e739c6d
Miguel Madrid Mencía <miguel.madrid-mencia@inserm.fr>
2019-09-10 15:15:51 +0200

    Add optional search parameter to the url

--- src/containers/FrontendContainer.ts
+++ src/containers/FrontendContainer.ts
@@ -1,20 +1,27 @@
 import { connect } from "react-redux";
-import { change_cell_type, change_organism } from "../actions/index";
+import {
+  change_cell_type,
+  change_organism,
+  change_search
+} from "../actions/index";
 import { Frontend } from "../components/Frontend";
 
 export const mapStateToProps = (state: {
   organism: string;
   cell_type: string;
+  search: string;
 }) => {
   return {
     cell_type: state.cell_type,
-    organism: state.organism
+    organism: state.organism,
+    search: state.search
   };
 };
 
 export const mapDispatchToProps = {
   onCellTypeChange: change_cell_type,
-  onOrganismChange: change_organism
+  onOrganismChange: change_organism,
+  onSearchChange: change_search
 };
 
 export const FrontendContainer = connect(
